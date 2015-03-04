7ebe4be9e4ffc0a94a58dbafbde01d5fbd9253bd
jfilm02 <jfilm@macmini-lab10.local>
2015-03-04 11:02:51 -0500

    i set up animation
    yeah

--- Farm.java
+++ Farm.java
@@ -4,9 +4,14 @@ public class Farm
  {
    World ref=new World();
    Cow steveyboy=new Cow(ref);
-    Pikachu pik = new Pikachu(wref);
-    pik.forward(100);
-    Pup mac = new Pup(wref);
-    mac.forward(100);
+   steveyboy.backward(100);
+   steveyboy.turn(-90);
+   steveyboy.forward(10);
+   steveyboy.animateForward();
+   steveyboy.animateTurn();
+   Pikachu pik = new Pikachu(ref);
+   pik.forward(100);
+   Pup mac = new Pup(ref);
+   mac.forward(100);
   }
 }
